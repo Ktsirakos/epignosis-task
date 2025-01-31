47fd72cf2ccdf64fea3ee403363ad45d270e6ae1
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-01-31 16:20:02 +0200

    feat: adding icon button component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <script setup lang="ts">
+import IconButton from "./components/IconButton.vue"
+import { RotateCcw, Save } from "lucide-vue-next";
+
+const iconSize = 72
 </script>
 
 <template>
-  <p class="text-red-500 text-5xl">Hello world</p>
+  <div class="flex flex-column gap-10">
+    <IconButton alt-text="Get a new joke" title="Reload" @click="() => { console.log('Clicked 1') }">
+      <template #icon>
+        <RotateCcw :size="iconSize" />
+      </template>
+    </IconButton>
+    <IconButton alt-text="Save joke" title="Save" @click="() => { console.log('Clicked 2') }">
+      <template #icon>
+        <Save :size="iconSize" />
+      </template>
+    </IconButton>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/IconButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="flex flex-col items-center gap-5 p-15 bg-black rounded-xl hover:bg-red-500" @click="props.onClick" :alt-text="props.altText">
+        <slot name="icon"></slot>
+        <p class="text-2xl text-center">{{ title }}</p>
+    </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    onClick: () => void
+    altText: string
+    title: string
+}>()
+</script>
+
+<style scoped></style>
