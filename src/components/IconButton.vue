e8a503de9edc224b89ead6e0f93746cc094fb76b
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-02 20:49:46 +0200

    feat: adidng disabled function on icon button

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -1,5 +1,7 @@
 <template>
-    <button class="flex flex-col items-center gap-5 p-15 bg-black rounded-xl hover:bg-red-500" @click="props.onClick" :alt-text="props.altText">
+    <button :disabled="disabled"
+        :class="`flex flex-col items-center gap-5 p-10 bg-black rounded-xl hover:bg-red-500 h-50 ${disabled ? 'opacity-20' : 'opacity-100'}`"
+        @click="props.onClick" :alt-text="props.altText">
         <slot name="icon"></slot>
         <p class="text-2xl text-center">{{ title }}</p>
     </button>
@@ -10,6 +12,7 @@ const props = defineProps<{
     onClick: () => void
     altText: string
     title: string
+    disabled?: boolean
 }>()
 </script>
 
