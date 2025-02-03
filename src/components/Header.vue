1dc7867106202556dd169ea9752c750681106f31
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-03 09:12:46 +0000

    feat: creating header component

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex flex-row justify-center gap-5 m-5">
+        <RouterLink :class="`'text-red' 
+        ${$route.path === '/' ? 'selected' : 'non-selected'}`" to="/">
+            Home
+        </RouterLink>
+        <RouterLink :class="$route.path === '/joke-list' ? 'selected' : 'non-selected'" to="/joke-list">
+            Saved Jokes
+        </RouterLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+
+<style scoped>
+.selected {
+    color: yellow
+}
+
+.non-selected {
+    color: gray
+}
+</style>
