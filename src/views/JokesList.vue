b53f62fafb2a94e4e7773b3cdc0b68934aab9ac1
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-03 11:00:45 +0000

    feat: creating joke list view

---
+++ src/views/JokesList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col gap-10">
+    <Search />
+    <div v-for="joke in jokeStore.savedJokes" class="flex flex-row justify-center">
+      <SavedJoke :joke="joke" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useJokeStore } from '../store/jokeStore';
+import SavedJoke from '@/components/SavedJoke.vue';
+
+const jokeStore = useJokeStore()
+</script>
