baccdd7a37a88def7840143272c0b02c235fcd20
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-03 12:06:53 +0000

    feat: adding search on saved jokes

---
+++ src/components/Search.vue
@@ -0,0 +1,14 @@
+<template>
+    <input v-model="query" class="rounded-xl text-white border-1 p-3 w-1/2" placeholder="Search your jokes"/>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const emit = defineEmits(['change'])
+const query = ref("")
+
+watch(query, (newQuery) => {
+    emit('change', newQuery)
+})
+</script>
--- src/views/JokesList.vue
+++ src/views/JokesList.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="flex flex-col gap-10">
-    <Search />
-    <div v-for="joke in jokeStore.savedJokes" class="flex flex-row justify-center">
+    <Search @change="(query) => filterJokes(query)" class="self-center mt-5" />
+    <div v-for="joke in filteredJokes" class="flex flex-row justify-center">
       <SavedJoke :joke="joke" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useJokeStore } from '../store/jokeStore';
+import { useJokeStore } from '@/store/jokeStore';
 import SavedJoke from '@/components/SavedJoke.vue';
+import Search from "@/components/Search.vue"
+import { ref } from 'vue';
+import type { Joke } from '@/interfaces/joke';
 
 const jokeStore = useJokeStore()
+const filteredJokes = ref([...jokeStore.savedJokes])
+
+const transformJokeToSearchableString = (joke: Joke) => {
+  return (
+    joke.setup.replace(" ", "").toLowerCase() +
+    joke.punchline.replace(" ", "").toLowerCase()
+  )
+}
+const filterJokes = (query: string) => {
+  if (!query) filteredJokes.value = [...jokeStore.savedJokes]
+  filteredJokes.value = jokeStore.savedJokes.filter(e => transformJokeToSearchableString(e).includes(query))
+}
 </script>
