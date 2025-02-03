12985ee0382e9e59cfb9be6b58e19ad03b501b1d
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2025-02-03 09:12:32 +0000

    chore: adding vue router

---
+++ src/router.ts
@@ -0,0 +1,16 @@
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+import Home from './views/Home.vue'
+import JokesList from './views/JokesList.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/joke-list', component: JokesList },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+export { router }
